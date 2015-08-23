49843b8a39b59680eb4479ad7509468af2243d10
pradil90 <pradil90@gmail.com>
2015-08-23 02:09:57 -0700

    initial

---
+++ index.php
@@ -0,0 +1,123 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
+<title>Pizza Village</title>
+<link rel="stylesheet" type="text/css" media="all" href="style.css" />
+
+<script type="text/javascript" src="style/js/jquery-1.7.2.min.js"></script>
+<script type="text/javascript" src="style/js/ddsmoothmenu.js"></script>
+<script type="text/javascript" src="style/js/retina.js"></script>
+<script type="text/javascript" src="style/js/selectnav.js"></script>
+<script type="text/javascript" src="style/js/jquery.masonry.min.js"></script>
+
+<script type="text/javascript" src="style/js/jquery.backstretch.min.js"></script>
+
+<script type="text/javascript">
+
+	$.backstretch("style/images/Supreme_pizza.jpg");
+</script>
+</head>
+<body>
+<div class="scanlines"></div>
+
+<!-- Begin Header -->
+<div class="header-wrapper opacity">
+	<div class="header">
+		<!-- Begin Logo -->
+		<div class="logo">
+		    <a href="index.html">
+				<B><i><div class="intro">Pizza Village</div></B></i>
+			</a>
+	    </div>
+		<!-- End Logo -->
+		<!-- Begin Menu -->
+		<div id="menu-wrapper">
+			<div id="menu" class="menu">
+				<ul id="tiny">
+					<li class="active"><a href="index.php">Home</a></li>
+						<!-- <ul>
+							<li><a href="index.php">Register</a></li>
+						</ul> -->
+					
+					
+					<li><a href="contact.php">Contact</a></li>
+				</ul>
+			</div>
+		</div>
+		<div class="clear"></div>
+		<!-- End Menu -->
+	</div>
+</div>
+<!-- End Header -->
+
+<!-- Begin Wrapper -->
+<center>
+<div class="wrapper"><!-- Begin Intro -->
+			<div class="intro">Make Your Own Pizza now in Seconds </div>
+				<form name="login-form" class="login-form" action="checklogin.php" method="post">
+					<!--  -->
+					<!-- <center><h2 class="title">Login</h2></center> -->
+					<!-- Begin Blog Grid -->
+					<div class="blog-wrap">
+							<!-- Begin Blog -->
+							<!-- <div class="blog-grid"> -->
+			
+					<div class="post format-standard box">
+
+					<h2 class="title">Login</h2>
+					<br>
+							User Name: <input type="text" name= "Username"> <br>
+					<pre>
+					</pre>
+							Password:   <input type="password" name = "password">
+					<pre>
+
+					</pre>
+					<input type="submit" name="Login" value="Login">
+	
+		
+		</div>
+	
+ 	
+ 	</div>
+
+ 	 	
+	<!-- End Blog -->
+</div></center><pre>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</pre>
+
+<div class="footer-wrapper">
+<div id="footer" class="four">
+		<div id="first" class="widget-area">
+			<div class="widget widget_search">
+				
+			</div>
+			
+	
+	</div>
+	<center><h1>Make your Own Pizza</h1></center>
+</div>
+<div class="site-generator-wrapper">
+	<div class="site-generator">Copyright CMPE 280. Design by <a href="">Pradeep Ivan Bosco,Alice George Thomas</a>. All rights reserved.</div>
+</div>
+<!-- End Footer --> 
+<script type="text/javascript" src="style/js/scripts.js"></script>
+</body>
+</html>
